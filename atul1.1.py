5d8721f97b95ad92dfa1c4b49e05754e4e2e5c6d
atulnagre1 <44189172+atulnagre1@users.noreply.github.com>
2018-10-16 12:04:42 +0530

    Add files via upload

---
+++ atul1.1.py
@@ -0,0 +1,90 @@
+Python 2.7.14 (v2.7.14:84471935ed, Sep 16 2017, 20:19:30) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+===================== RESTART: C:\Python27\atul.pyXYZ.py =====================
+banana
+>>> 
+===================== RESTART: C:\Python27\atul.pyXYZ.py =====================
+banana
+apple
+banana
+cherry
+>>> 
+===================== RESTART: C:\Python27\atul.pyXYZ.py =====================
+banana
+apple
+banana
+cherry
+cherry
+>>> 
+===================== RESTART: C:\Python27\atul.pyXYZ.py =====================
+banana
+apple
+banana
+cherry
+cherry
+0
+1
+2
+3
+4
+5
+['finally finished']
+>>> 
+===================== RESTART: C:\Python27\atul.pyXYZ.py =====================
+banana
+apple
+banana
+cherry
+cherry
+0
+1
+2
+3
+4
+5
+['finally finished']
+the end
+s
+the end
+t
+the end
+r
+the end
+n
+the end
+g
+>>> 
+===================== RESTART: C:\Python27\atul.pyXYZ.py =====================
+banana
+apple
+banana
+cherry
+cherry
+0
+1
+2
+3
+4
+5
+['finally finished']
+the end
+s
+the end
+t
+the end
+r
+the end
+n
+the end
+g
+('red', 'apple')
+('red', 'banana')
+('red', 'cherry')
+('big', 'apple')
+('big', 'banana')
+('big', 'cherry')
+('tasty', 'apple')
+('tasty', 'banana')
+('tasty', 'cherry')
+>>> 
---
+++ atul1.py
@@ -0,0 +1,40 @@
+fruits=["apple","banana","cherry"]
+for x in fruits:
+    if x=="banana":
+        break
+print (x)
+
+Fruits=["apple","banana","cherry"]
+for x in fruits:
+    print(x)
+
+
+Fruits=["apple","banana","cherry"]
+for x in fruits:
+    if x=="banana":
+        continue
+print(x)
+
+for x in range(6):
+    print (x)
+else:
+    print["finally finished"]
+
+for val in "string":
+    if val=="i":
+        continue
+    print("the end")
+    print(val)
+
+adj=["red","big","tasty"]
+fruits=["apple","banana","cherry"]
+for x in adj:
+    for y in fruits:
+        print(x,y)
+        
+
+
+        
+    
+        
+    
